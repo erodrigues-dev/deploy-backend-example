ba5a8da5853fd60d3dcc13293e33010e713bb080
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-10-26 20:17:06 -0300

    feat: adiciona log

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
-app.get('/', (_, res) => res.json({
+app.get('/', (_, res) => {
+
+  console.log(`[${new Date().toISOString()}] >> GET / `)
+
+  return res.json({
   ok: true,
   message: 'server is running'
-}))
+})})
 
 app.listen(port, () => {
   console.log(`Server runing in port: ${port}`)
