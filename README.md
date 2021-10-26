163ee340b1b700f56ddde9c46069eb8c886beef7
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-10-26 19:58:11 -0300

    cria servidor nodejs com express

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# deploy-backend-example
+# deploy backendexample
 Usando github actions para fazer deploy de uma aplicação nodejs
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app =  express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (_, res) => res.json({
+  ok: true,
+  message: 'server is running'
+}))
+
+app.listen(port, () => {
+  console.log(`Server runing in port: ${port}`)
+})
